b0e6a670e42eb81903e5af07a1f3dbdba29d2503
Amal Manoj <amalmanoj02@gmail.com>
2023-05-03 10:15:09 +0530

    react app created

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Amal Manoj
+      </h1>
+      <input type="text" name="Username" placeholder="Username" id="username"></input><br></br><br></br>
+      <button >Submit</button>
+    </div>
+  );
+}
+
+export default App;
